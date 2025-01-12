51cc296ecc82ec5e940605c720c01d07bb56602b
Kulakov Alexey <kulakov@gmail.com>
2025-01-12 23:18:22 +0100

    Обновлен код для обработки вебхуков

--- config.py
+++ config.py
@@ -1,4 +1,12 @@
+from dotenv import load_dotenv
 import os
+
+# Загружаем переменные окружения из .env файла
+load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 SPREADSHEET_ID = os.getenv("SPREADSHEET_ID")
+
+# Для отладки: убедитесь, что переменные загружены корректно
+print("BOT_TOKEN:", BOT_TOKEN)
+print("SPREADSHEET_ID:", SPREADSHEET_ID)
